f9640137fe2e8900d8358e17eb901a1140514c7d
chikien2412 <chikien2412@gmail.com>
2021-10-22 09:08:11 +0700

    Danh ba dien thoai

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Danh ba dien tu</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <!-- Font-awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.1.0-10/css/all.css">
+    <!-- person css -->
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="row">
+        <div class="col-sm-8 push-sm-2">
+            <form>
+                <div class="container">
+                    <!-- <div class="card-body">
+                        Danh ba dien tu 
+                    </div> -->
+                    <div class="mb-3">
+                        <label for="formGroupExampleInput" class="form-label">Email</label>
+                        <input type="text" class="form-control" id="formGroupExampleInput"
+                            placeholder="">
+                    </div>
+                    <div class="form-group">
+                        <label for="formGroupExampleInput2" class="form-label">SĐT</label>
+                        <input type="text" class="form-control" id="formGroupExampleInput2"
+                            placeholder="">
+                    </div>
+                    <div class="mb-3">
+                        <label for="formGroupExampleInput2" class="form-label">Họ và tên</label>
+                        <input type="text" class="form-control" id="formGroupExampleInput2"
+                            placeholder="">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
